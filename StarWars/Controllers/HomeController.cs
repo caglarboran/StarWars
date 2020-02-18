76d2e4cac9fbb3be2508e704e958f41558ae1568
Çağlar Boran <caglar.boran@hotmail.com>
2020-02-18 23:50:34 +0100

    Starwars

---
+++ StarWars/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+
+namespace StarWars.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Actions
+        public ActionResult Index() 
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
